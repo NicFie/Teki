79129775f9a9ad441eabb9d2d3f92c27fc99bdf8
Nicola Field <nicola.field2@gmail.com>
2022-09-29 16:09:00 +0100

    Tidying up fading in javascript

--- app/javascript/controllers/lp_cards_controller.js
+++ app/javascript/controllers/lp_cards_controller.js
@@ -4,16 +4,12 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["card"];
   connect() {
-    console.log('lp-card connected')
   }
 
   changeOpacity() {
-    const scroll = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
-    this.cardTargets.forEach((card) => {
+      this.cardTargets.forEach((card) => {
       const distInView = card.getBoundingClientRect().top - window.innerHeight + 20;
       if (distInView < 0) {
-        console.log(card)
         card.classList.add("inView");
     } else {
         card.classList.remove("inView");
