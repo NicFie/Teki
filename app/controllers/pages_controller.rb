d102334f4db034f479b1ae764bbaeb352091329d
will dunwoody <will.dunwoody@gmail.com>
2022-10-26 20:07:29 +0200

    started writing iterators to iterate through each game where a player has played

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,6 +7,11 @@ class PagesController < ApplicationController
   def dashboard
     @users_ordered_by_score = User.where('id != 1').order('score DESC').all
     @friendships = Friendship.where(asker_id: current_user)
+    as_user_one = current_user.games_as_player_one
+    as_user_two = current_user.games_as_player_two
+    @latest_battles = [as_user_one, as_user_two].flatten.sort.reverse
+
+    raise
     @game = Game.new
   end
 end
