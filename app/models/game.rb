482b22e653cf22f9174b13bbc8cb0df6d132e41f
will dunwoody <will.dunwoody@gmail.com>
2022-10-27 10:17:51 +0200

    added table for finished games

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,6 @@ class Game < ApplicationRecord
   has_many :game_rounds
   belongs_to :player_one, class_name: "User"
   belongs_to :player_two, class_name: "User"
+
+  # scope :with_game_winners, -> { where("game_winner != nil") }
 end
