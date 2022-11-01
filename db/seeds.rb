984a577bb94133457e816f56649f0c922ea07bf6
Aaron Moores <mooresaaron@gmail.com>
2022-11-01 16:02:21 +0000

    dash changes and seeds fixed and tidied

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ puts "creating Katas erm... Challenges !"
 NotEvenOdd = Challenge.create(
   name: 'Not even odd',
   description: 'Write a function taking a number as an argument.
-  \nThe function should return "even steven" if the number is even and "that was odd..." if the number is odd',
+  The function should return "even steven" if the number is even and "that was odd..." if the number is odd',
   language: 'Ruby',
   tests: {
     4 => 'even steven',
@@ -20,12 +20,12 @@ NotEvenOdd = Challenge.create(
 ArrayArrayArray = Challenge.create(
   name: 'Array Array Array',
   description: 'You are given an initial 2-value array. You will use this to calculate a score.
-  \nIf both values in the array are numbers, the score is the sum of the two.
-  \nIf only one is a number, the score is that number.
-  \nIf neither is a number, return "Void!".
-  \nOnce you have your score, you must return an array of arrays.
-  \nEach sub array will be the same as the original array and the number of sub arrays should be equal to the score.
-  \n\nFor example: if (array) == ["a", 3] \nyou should return [["a", 3], ["a", 3], ["a", 3]].',
+  If both values in the array are numbers, the score is the sum of the two.
+  If only one is a number, the score is that number.
+  If neither is a number, return "Void!".
+  Once you have your score, you must return an array of arrays.
+  Each sub array will be the same as the original array and the number of sub arrays should be equal to the score.
+  For example: if (array) == ["a", 3] you should return [["a", 3], ["a", 3], ["a", 3]].',
   language: 'Ruby',
   tests: {
     ['a', 3] => [['a', 3], ['a', 3], ['a', 3]],
@@ -38,14 +38,14 @@ PrintedErrors = Challenge.create(
   name: 'Printed errors',
   description: 'In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors
   which, are named with letters from a to m.
-  \nThe colors used by the printer are recorded in a control string.
-  \nFor example a "good" control string would be aaabbbbhaijjjm meaning that the printer used color a three times, color b four times, color h once, then
+  The colors used by the printer are recorded in a control string.
+  For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used color a three times, color b four times, color h once, then
   once again color a...
-  \nSometimes there are problems: lack of colors, technical malfunctions, and a "bad" control string is produced for example:
-  \naaaxbbbbyyhwawiwjjjwwm which includes letters outside of a-m.
-  \n\nYou have to write a function which given a string will return the error rate of the printer as a string with a numerator showing the number of errors, and a denominator showing the length of the control string.
-  \nFor example:
-  \n"1/15" (1 error, 15 control string length) Do not reduce this fraction to a simpler expression.',
+  Sometimes there are problems: lack of colors, technical malfunctions, and a "bad" control string is produced for example:
+  aaaxbbbbyyhwawiwjjjwwm which includes letters outside of a-m.
+  You have to write a function which given a string will return the error rate of the printer as a string with a numerator showing the number of errors, and a denominator showing the length of the control string.
+  For example:
+  "1/15" (1 error, 15 control string length) Do not reduce this fraction to a simpler expression.',
   language: 'Ruby',
   tests: {
     'aaabbbbhaijjjm' => '0/14',
@@ -57,12 +57,14 @@ PrintedErrors = Challenge.create(
 Arrays = Challenge.create(
   name: 'Arrays',
   description: '
-  You are given an array (which will have a length of at least 3, but could be very large) containing integers. \nThe array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. \nWrite a method that takes the array as an argument and returns this "outlier" N.
-  \n\nExamples
-  \n[2, 4, 0, 100, 4, 11, 2602, 36]
-  \nShould return: 11 (the only odd number)
-  \n[160, 3, 1719, 19, 11, 13, -21]
-  \nShould return: 160 (the only even number)',
+  You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+  The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+  Write a method that takes the array as an argument and returns this "outlier" N.
+  Examples
+  [2, 4, 0, 100, 4, 11, 2602, 36]
+  Should return: 11 (the only odd number)
+  [160, 3, 1719, 19, 11, 13, -21]
+  Should return: 160 (the only even number)',
   language: 'Ruby',
   tests: {
     [2, 4, 0, 100, 4, 11, 2602, 36] => 11,
@@ -73,10 +75,11 @@ Arrays = Challenge.create(
 
 SortNumbers = Challenge.create(
   name: 'Sort numbers',
-  description: 'You are given an array of integers. \nYour task is to sort odd numbers within the array in ascending order, and even numbers in descending order.
-  \nNote that zero is an even number. If you have an empty array, you need to return it.
-  \n\nFor example:
-  \n[5, 3, 2, 8, 1, 4]  -->  [1, 3, 5, 8, 4, 2]',
+  description: 'You are given an array of integers.
+  Your task is to sort odd numbers within the array in ascending order, and even numbers in descending order.
+  Note that zero is an even number. If you have an empty array, you need to return it.
+  For example:
+  [5, 3, 2, 8, 1, 4]  -->  [1, 3, 5, 8, 4, 2]',
   language: 'Ruby',
   tests: {
     [5, 3, 2, 8, 1, 4] => [1, 3, 5, 8, 4, 2],
@@ -89,11 +92,11 @@ DescendingOrder = Challenge.create(
   name: 'Descending order',
   description: 'Your task is to make a function that can take any non-negative integer as an argument and return
   it with its digits in descending order.
-  \n
+
   Examples:
-  \nInput: 42145 Output: 54421
-  \nInput: 145263 Output: 654321
-  \nInput: 123456789 Output: 987654321',
+  Input: 42145 Output: 54421
+  Input: 145263 Output: 654321
+  Input: 123456789 Output: 987654321',
   language: 'Ruby',
   tests: {
     42145 => 54421,
