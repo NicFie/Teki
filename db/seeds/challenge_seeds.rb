6fda7f7d56ff47dd8c5cf2e9c03017e5b2aec07d
Nicola Field <nicola.field2@gmail.com>
2022-11-07 14:29:02 +0000

    Adding 10 more medium level challenges

--- db/seeds/challenge_seeds.rb
+++ db/seeds/challenge_seeds.rb
@@ -154,3 +154,223 @@ MultiplyNumbersInAString = Challenge.create(
   },
   method_template: 'def multiply_numbers_in_a_string(string)\n  \nend'
 )
+
+LastLetterSort = Challenge.create(
+  name: 'Last Letter Sort',
+  description: 'Given a string of words, sort it alphabetically by the last character of each word.
+  If two words have the same last character, sort by the order they originally appeared.
+  The method should return an string of words.
+
+  For example:
+    "herb camera dynamic" --> "camera herb dynamic"
+    "brick moral institution loud talk resign worth" --> "loud worth brick talk moral institution resign"',
+  language: 'Ruby',
+  tests: {
+    "stab traction artist approach" => "stab approach traction artist",
+    "sample partner autonomy swallow trend" => "trend sample partner swallow autonomy",
+    "introduce fashionable cause sacrifice reality" => "introduce fashionable cause sacrifice reality"
+  },
+  method_template: 'def multiply_numbers_in_a_string(string)\n  \nend'
+)
+
+# These are some medium challenges
+
+TypeOfTriangle = Challenge.create(
+  name: 'Type of Triangle',
+  description: 'Given an array of the side lengths of a triangle, determine its type.
+
+  No sides equal: "scalene"
+  Two sides equal: "isosceles"
+  All sides equal: "equilateral"
+  Less or more than 3 sides given: "not a triangle"
+
+  The method should return a string.
+
+  For example:
+  [2, 6, 5] --> "scalene"
+  [4, 4, 7] --> "isosceles"
+  [3, 5, 5, 2] --> "not a triangle"',
+  language: 'Ruby',
+  tests: {
+    [2, 3, 4] => "scalene",
+    [4, 4, 7] => "isosceles",
+    [8, 8, 8] => "equilateral",
+    [10] => "not a triangle"
+  },
+  method_template: 'def type_of_triangle(integer)\n  \nend'
+)
+
+ReverseOddLengthWords = Challenge.create(
+  name: 'Reverse Odd Length Words',
+  description: 'Given a string, reverse all the words which have odd length. The even length words are not changed.
+
+  For example:
+  "Bananas" --> "sananaB"
+  "One two three four" --> "enO owt eerht four"
+  "Make sure uoy only esrever sdrow of ddo length" --> "Make sure you only reverse words of odd length"',
+  language: 'Ruby',
+  tests: {
+    "Even even even even even even even even even" => "Even even even even even even even even even",
+    "Odd Odd odd" => "ddO ddO ddo",
+    "Make sure you only reverse words of odd length" => "Make sure uoy only esrever sdrow of ddo length",
+  },
+  method_template: 'def reverse_odd_length_words(integer)\n  \nend'
+)
+
+PositionInAlphabet = Challenge.create(
+  name: 'Position in Alphabet',
+  description: 'Given a number between 1-26, return what letter is at that position in the alphabet. Return "invalid" if the number given is not within that range, or isn\'t an integer.
+
+  For example:
+  1 --> "a"
+  26.0 --> "z"
+  0 --> "invalid"',
+  language: 'Ruby',
+  tests: {
+    4 => "d",
+    9 => "i",
+    -1 => "invalid",
+    4.5 => "invalid",
+    21.0 => "u"
+  },
+  method_template: 'def position_in_alphabet(integer)\n  \nend'
+)
+
+FindBob = Challenge.create(
+  name: 'Find Bob',
+  description: 'Given an array of names, find Bob.
+  Return his location in the array, or -1 if Bob is not there. This method returns an integer
+
+  For example:
+  ["Will", "Nicola", "Bob"] --> 2
+  ["Bob", "Nicola", "Aaron", "Dareos"] --> 0
+  ["Will", "Nicola", "Aaron"] --> -1',
+  language: 'Ruby',
+  tests: {
+    ["Jimmy", "Layla", "Mandy"] => -1,
+    ["Bob", "Nathan", "Hayden"] => 0,
+    ["Paul", "Layla", "Bob"] => 2,
+    ["Garry", "Maria", "Bethany", "Bob", "Pauline"] => 3
+  },
+  method_template: 'def find_bob(array)\n  \nend'
+)
+
+LetterIncrement = Challenge.create(
+  name: 'Letter increment',
+  description: 'Given a one word string, write a method that changes every letter to the next letter. Ignore any punctuation.
+  This method returns a string.
+
+  For example:
+  "a" --> "b"
+  "bye!" --> "czf!"
+  "Welcome" --> "Xfmdpnf"',
+  language: 'Ruby',
+  tests: {
+    "Hello" => "Ifmmp",
+    "lol!?" => "mpm!?",
+    "bye" => "czf"
+  },
+  method_template: 'def letter_increment(string)\n  \nend'
+)
+
+Decimator = Challenge.create(
+  name: 'Decimator',
+  description: 'Write a DECIMATOR function which takes a string and decimates it (i.e. it removes the last 1/10 of the characters).
+  Always round up: if the string has 21 characters, 1/10 of the characters would be 2.1 characters, hence the DECIMATOR removes 3 characters. The DECIMATOR shows no mercy!
+
+  For example:
+  "1234567890" --> "123456789"
+  # 10 characters, removed 1.
+
+  "123" --> "12"
+  # 3 characters, removed 1
+
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZ" --> "ABCDEFGHIJKLMNOPQRSTUVW"
+  # 26 characters, removed 3.',
+  language: 'Ruby',
+  tests: {
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" => "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrst",
+    "A" => "",
+    "1234567890AB" => "1234567890",
+    "" => ""
+  },
+  method_template: 'def decimator(string)\n  \nend'
+)
+
+BinaryZeros = Challenge.create(
+  name: 'Binary Zeroes',
+  description: 'Given a binary string, write a function that returns the longest sequence of consecutive zeroes in a binary string.
+  If no zeroes exist in the input, return an empty string.
+  This method returns a string.
+
+  For example:
+  "01100001011000" --> "0000"
+  "100100100" --> "00"
+  "11111" --> ""',
+  language: 'Ruby',
+  tests: {
+    "1000000000011101" => "0000000000",
+    "100001110000100000" => "00000",
+    "101010101" => "0",
+    "111111" => ""
+  },
+  method_template: 'def binary_zeroes(string)\n  \nend'
+)
+
+SquaresAndCubes = Challenge.create(
+  name: 'Squares and Cubes',
+  description: 'Given an array of two numbers, check if the square root of the first number is equal to the cube root of the second number.
+  This method returns a boolean.
+
+  For example:
+  [4, 8] --> true
+  [16, 48] --> false
+  [9, 27] --> true',
+  language: 'Ruby',
+  tests: {
+    [5, 12] => false,
+    [25, 125] => true,
+    [1, 1] => true,
+    [36, 217] => false,
+    [9, 27] => true
+  },
+  method_template: 'def binary_zeroes(array)\n  \nend'
+)
+
+FindOddInteger = Challenge.create(
+  name: 'Find Odd Integer',
+  description: 'Create a function that takes an array and finds the integer which appears an odd number of times.
+  This method returns an integer.
+
+  For example:
+  [1, 1, 2, -2, 5, 2, 4, 4, -1, -2, 5] --> -1
+  [20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5] --> 5
+  [10] --> 10',
+  language: 'Ruby',
+  tests: {
+    [20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5] => 5,
+    [1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1] => 10,
+    [20, 1, 1, 2, 2, 3, 3, 5, 5, 4, 20, 4, 5] => 5,
+    [10] => 10
+  },
+  method_template: 'def find_odd_integer(array)\n  \nend'
+)
+
+NoYelling = Challenge.create(
+  name: 'No Yelling',
+  description: 'Create a function that transforms sentences ending with multiple question marks ? or exclamation marks ! into a sentence only ending with one without changing punctuation in the middle of the sentences.
+  This method returns a string.
+
+  For example:
+  "What went wrong?????????" --> "What went wrong?"
+  "Oh my goodness!!!" --> "Oh my goodness!"
+  "I just can\'t believe!!!!!!! it." --> "I just can\'t believe!!!!!!! it."',
+  language: 'Ruby',
+  tests: {
+    "What went wrong?????????" => "What went wrong?",
+    "Oh my goodness!!!" => "Oh my goodness!",
+    "WHAT!" => "WHAT!",
+    "That's a ton!! of cheese!!!!!!!!" => "That's a ton!! of cheese!"
+  },
+  method_template: 'def no_yelling(array)\n  \nend'
+)
