9a8e97b1f6cbd267722df9ef129928d374206180
will dunwoody <will.dunwoody@gmail.com>
2022-12-30 16:16:08 +0100

    created game_test method in game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -21,4 +21,57 @@ class Game < ApplicationRecord
   def self.existing_game(game)
     Game.where("player_two_id = ? and round_count = ?", 1, game["round_count"].to_i)
   end
+
+  def test_game(submission_code)
+    passed = []
+    output = []
+
+    begin
+      submission = eval(submission_code)
+    rescue SyntaxError => e
+      p "Syntax Error #{e}"
+      output << "<span style=\"color: #ffe66d; font-weight: bold;\">ERROR:</span> #{e.message.gsub!('(eval):3:', '')}"
+    # tests variable needs modifying to return not just first test but sequentially after round is won
+    else
+      game_tests = game_rounds.where('winner_id = 1').first.challenge.tests
+      tests = eval(game_tests)
+      display_keys = eval(game_tests).keys
+
+      count = 0
+
+      tests.each do |k, v|
+        count += 1
+        begin
+          p "Arriving in call"
+          call = method(submission).call(k)
+          p "Call is #{call}"
+        rescue StandardError => e
+          p "Standard Error #{e.message}"
+          output << "<span style=\"color: #ffe66d; font-weight: bold;\">ERROR:</span> #{e.message.gsub!(/for #| for \d:\w+\W+\w+.+\W+/, '')}<br><br>"
+        rescue ScriptError => e
+          p "Script Error #{e}"
+          output << "<span style=\"color: #ffe66d; font-weight: bold;\">ERROR:</span> #{e.message.gsub!(/(for #<\w+:\w+>\s+\w+\s+\^+|for #<\w+:\w+>)/, '')}<br><br>"
+        else
+          if call == v
+            passed << true
+            output << "#{count}. <span style=\"color: green; font-weight: bold;\">Test passed:</span><br>When given #{display_keys[count - 1]}, method successfully returned #{v}.<br><br>"
+          else
+            passed << false
+            output << "#{count}. <span style=\"color: #ff6346; font-weight: bold;\">Test failed:</span><br> Given: #{display_keys[count - 1]}. Expected: #{v.class == String ? "'#{v}'" : v}. Got: #{
+              if call.nil?
+                "nil"
+              elsif call.class == String
+                "'#{call}'"
+              elsif call.class == Symbol
+                ":#{call}"
+              else
+                call
+              end
+            }.<br><br>"
+          end
+        end
+      end
+
+      { output: output.join, all_passed: passed }
+    end
 end
