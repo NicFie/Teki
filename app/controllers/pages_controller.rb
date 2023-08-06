1931dc7707c9fbf27416f6782368f50b8b88c7c5
Will Dunwoody <will.dunwoody@gmail.com>
2023-08-06 12:31:48 +0200

    Refactored dashboard MVC Moved all logic out of controller and views and put them in game and user models. Created scope for leaderboard Created instance methods for player info Changed view and created partials for each section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,16 +5,6 @@ class PagesController < ApplicationController
   end
 
   def dashboard
-    @users_ordered_by_score = User.where('id != 1').order('score DESC').all
-    @friends = current_user.friends.order('updated_at DESC')
-    users_games = Game.where(
-                        player_one_id: current_user.id).where.not(game_winner: nil)
-                      .or(Game.where(
-                        player_two_id: current_user.id).where.not(game_winner: nil))
-                      .order(updated_at: :desc)
-    @latest_battles = users_games.to_a
-    @requests = current_user.pending_invitations
-    @with_friend = false
     @game = Game.new
   end
 
