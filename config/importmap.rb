f15d70869598387911f55da2444a739a13b1270c
Will Dunwoody <will.dunwoody@gmail.com>
2023-11-12 07:12:07 +0100

    updated to rails 7 added some importmapts
    TODO: Get codemirror working

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "typed.js", to: "https://ga.jspm.io/npm:typed.js@2.1.0/dist/typed.module.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "actioncable", to: "https://ga.jspm.io/npm:actioncable@5.2.8-1/lib/assets/compiled/action_cable.js"
+pin "@rails/actioncable", to: "https://ga.jspm.io/npm:@rails/actioncable@7.1.2/app/assets/javascripts/actioncable.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
