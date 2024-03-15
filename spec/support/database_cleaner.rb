dd6cbe885cd05ac78f65e0460c736029ef257c98
Will Dunwoody <will.dunwoody@gmail.com>
2024-03-15 15:53:04 +0100

    updated configuration for databasecleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -4,6 +4,7 @@ require 'database_cleaner/active_record'
 
 RSpec.configure do |config|
   config.before(:suite) do
+    DatabaseCleaner.url_allowlist = %w[postgresql://will:teki_password@localhost:5432/teki_test postgres://postgres:postgres@localhost/teki_test]
     DatabaseCleaner.clean_with(:truncation)
     DatabaseCleaner.strategy = :transaction
 
