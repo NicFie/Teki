f5fd52d47fbb85d86de099274884d15c75b58827
Will Dunwoody <will.dunwoody@gmail.com>
2024-03-22 16:28:20 +0100

    fixed rubocop on tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -16,10 +16,13 @@ RSpec.describe Game do
 
   describe 'scopes' do
     describe '.existing_game' do
+      let(:player_one) { create(:user) }
+      let(:player_two) { create(:user) }
+
       it 'returns games with the given round_count and player_two_id = 1' do
         existing_game = create(:game, player_two_id: 1, round_count: 3)
-        create(:game, player_two_id: 2, round_count: 3)
-        create(:game, player_two_id: 1, round_count: 5)
+        create(:game, player_two_id: player_two.id, round_count: 3)
+        create(:game, player_two_id: player_one.id, round_count: 5)
 
         expect(described_class.existing_game({ 'round_count' => 3, 'player_two_id' => 1 })).to eq([existing_game])
       end
@@ -32,12 +35,17 @@ RSpec.describe Game do
 
   describe '#add_rounds_and_challenges' do
     let(:game) { create(:game, round_count: 3) }
-    let!(:challenges) { create_list(:challenge, 5) }
 
-    it 'creates the specified number of game rounds with challenges and sets a winner' do
+    before do
+      create_list(:challenge, 5)
       game.add_rounds_and_challenges
+    end
 
+    it 'creates the specified number of game rounds' do
       expect(game.game_rounds.count).to eq(3)
+    end
+
+    it 'sets an automatic winner for each round' do
       expect(game.game_rounds.pluck(:winner_id)).to all(eq(1))
     end
   end
